346f8f25d36ef0b9b2dfbe4f58ab1df87d273c80
Dana Kulp <dkulp23@gmail.com>
2016-11-29 19:28:28 -0800

    We have the AJAX request for the JSON data working and storing to local storage. We are working on getting the articles to render in the DOM now.

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -49,16 +49,25 @@ Article.fetchAll = function() {
     /* When our data is already in localStorage:
     1. We can process and load it,
     2. Then we can render the index page.  */
+    // var localStorageData = localStorage.getItem(JSON.parse('hackerIpsum'));
+
   } else {
     /* Without our localStorage in memory, we need to:
     1. Retrieve our JSON file with $.getJSON
       1.a Load our json data
       1.b Store that data in localStorage so that we can skip the server call next time,
       1.c And then render the index page.*/
+    $.getJSON('data/hackerIpsum.json', function(data){
+      localStorage.setItem('hackerIpsumObjects', JSON.stringify(data));
+      Article.loadAll(data);
+      Article.allArticles.forEach(function(element){
+        element.toHtml('#article-template');
+      });
+    })
   }
 };
 
-
+Article.fetchAll();
 
 /* Great work so far! STRETCH GOAL TIME!? Refactor your fetchAll above, or
    get some additional typing practice below. Our main goal in this part of the
